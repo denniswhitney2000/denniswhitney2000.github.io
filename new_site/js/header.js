345038f2f2c479d5ad21e5b6b762f3640a1468cb
denniswhitney2000 <dwhitney985@gmail.com>
2024-12-01 22:31:29 -0500

    updating js paths

--- new_site/js/header.js
+++ new_site/js/header.js
@@ -6,43 +6,10 @@ document.addEventListener('DOMContentLoaded', function() {
         baseUrl = '../'.repeat(pathArray.length - 2);
     }
     
-    fetch(baseUrl + 'html/header.html')
+    fetch(baseUrl + '../html/header.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('header-placeholder').innerHTML = data;
         })
         .catch(error => console.error('Error loading header:', error));
 });
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     // Determine the base URL based on the current location
-//     const baseUrl = window.location.pathname.split('/').length > 2 ? '../' : './';
-    
-//     fetch(baseUrl + 'html/header.html')
-//         .then(response => response.text())
-//         .then(data => {
-//             document.getElementById('header-placeholder').innerHTML = data;
-//         })
-//         .catch(error => console.error('Error loading header:', error));
-// });
-
-// // document.addEventListener('DOMContentLoaded', function() {
-// //     // Determine the base URL based on the current location
-// //     const baseUrl = window.location.pathname.includes('/') ? '../' : './';
-    
-// //     fetch(baseUrl + 'html/header.html')
-// //         .then(response => response.text())
-// //         .then(data => {
-// //             document.getElementById('header-placeholder').innerHTML = data;
-// //         })
-// //         .catch(error => console.error('Error loading header:', error));
-// // });
-
-// // // document.addEventListener('DOMContentLoaded', function() {
-// // //     fetch('html/header.html')
-// // //         .then(response => response.text())
-// // //         .then(data => {
-// // //             document.getElementById('header-placeholder').innerHTML = data;
-// // //         })
-// // //         .catch(error => console.error('Error loading header:', error));
-// // // });
