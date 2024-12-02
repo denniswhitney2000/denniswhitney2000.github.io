b6d7c0d9193b4524869458019e61af997499a404
denniswhitney2000 <dwhitney985@gmail.com>
2024-12-02 02:02:55 -0500

    1 more time

--- new_site/js/footer.js
+++ new_site/js/footer.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Determine the base URL based on the current location
     const baseUrl = window.location.pathname.includes('/') ? '../' : './';
     
-    fetch(baseUrl + '../html/footer.html')
+    fetch(baseUrl + 'html/footer.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('footer-placeholder').innerHTML = data;
