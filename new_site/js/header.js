b3b02387039ad2e24a87f3c162c41ae5d299006f
denniswhitney2000 <dwhitney985@gmail.com>
2024-12-02 10:55:10 -0500

    keep swimming

--- new_site/js/header.js
+++ new_site/js/header.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
         baseUrl = '../'.repeat(pathArray.length - 2);
     }
     
-    fetch(baseUrl + 'html/header.html')
+    fetch(baseUrl + '/new_site/html/header.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('header-placeholder').innerHTML = data;
--- new_site/js/nav.js
+++ new_site/js/nav.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Determine the base URL based on the current location
     const baseUrl = window.location.pathname.includes('/') ? '../' : './';
     
-    fetch(baseUrl + 'html/nav.html')
+    fetch(baseUrl + '/new_site/html/nav.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('nav-placeholder').innerHTML = data;
