2a69417b701a96a87a7fdb1af0d0b821183df8ee
denniswhitney2000 <dwhitney985@gmail.com>
2024-12-02 11:02:45 -0500

    moved out fo the html dir

--- new_site/js/footer.js
+++ new_site/js/footer.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Determine the base URL based on the current location
     const baseUrl = window.location.pathname.includes('/') ? '../' : './';
     
-    fetch(baseUrl + '/new_site/html/footer.html')
+    fetch(baseUrl + 'footer.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('footer-placeholder').innerHTML = data;
